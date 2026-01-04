e6faae4eb950397895a5fe5f2b6908ebb3bd0e8a
Dimas Mayoni <dimasmayoni8829@gmail.com>
2026-01-04 14:16:07 +0700

    14

--- src/auth.js
+++ src/auth.js
@@ -1,27 +1,21 @@
 import { auth } from "./firebase.js";
-import {
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const form = document.getElementById("loginForm");
 const errorText = document.getElementById("errorText");
-
-onAuthStateChanged(auth, (user) => {
-  if (user) location.href = "dashboard.html";
-});
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   errorText.textContent = "";
 
+  const email = form.email.value;
+  const password = form.password.value;
+
   try {
-    await signInWithEmailAndPassword(
-      auth,
-      form.email.value,
-      form.password.value
-    );
+    await signInWithEmailAndPassword(auth, email, password);
+    location.href = "dashboard.html";
   } catch (err) {
-    errorText.textContent = err.message;
+    console.error(err);
+    errorText.textContent = "Email atau password salah";
   }
 });
