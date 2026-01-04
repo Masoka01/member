3da1499be491ac281915fc0808a4bdb2bec9d565
Dimas Mayoni <dimasmayoni8829@gmail.com>
2026-01-04 14:05:37 +0700

    13

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIxjtnIJEM-p_0_uCwHjTzYx9Eaoz0yhE",
+  apiKey: "AIzaSyCIxitnJEM-p_0_ucWHIzYx9Eaoz0vhE",
   authDomain: "absensi-project-0.firebaseapp.com",
   projectId: "absensi-project-0",
-  storageBucket: "absensi-project-0.firebasestorage.app",
+  storageBucket: "absensi-project-0.appspot.com",
   messagingSenderId: "1000393480842",
   appId: "1:1000393480842:web:f36db120025c51cf312b73",
-  measurementId: "G-PGKZDE79D3",
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
